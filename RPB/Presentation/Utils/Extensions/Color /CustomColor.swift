2ffd8da59cb9a8d12f148c15fc3a9f9ed9e6570a
telha.wasim <telha.wasim@synavos.com>
2022-12-23 13:13:27 +0500

    merge conflicts

--- RPB/Presentation/Utils/Extensions/Color /CustomColor.swift
+++ RPB/Presentation/Utils/Extensions/Color /CustomColor.swift
@@ -12,7 +12,6 @@ extension UIColor {
     static let customBlack = UIColor(red: 34.0/255.0, green: 34.0/255.0, blue: 34.0/255.0, alpha: 1.0)
     static let customBlue = UIColor(red: 85.0/255.0, green: 87.0/255.0, blue: 212.0/255.0, alpha: 1.0)
     static let customYellow = UIColor(red: 238.0/255.0, green: 243.0/255.0, blue: 106.0/255.0, alpha: 1.0)
-    static let customBlue = UIColor(red: 85.0/255.0, green: 87.0/255.0, blue: 212.0/255.0, alpha: 1.0)
 }
 
 // Example
