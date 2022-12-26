ac6979c852eccf8564b90613edbe5fe44b4f8d15
nomanr2 <nomanakramesv@gmail.com>
2022-12-26 10:54:39 +0500

    Setting VC

--- RPB/Presentation/Setting/SettingVC.swift
+++ RPB/Presentation/Setting/SettingVC.swift
@@ -9,8 +9,40 @@ import UIKit
 
 class SettingVC: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var lblSetting: UILabel!
+    @IBOutlet weak var lblAccount: UILabel!
+    @IBOutlet weak var lblEditProfile: UILabel!
+    @IBOutlet weak var lblNotification: UILabel!
+    @IBOutlet weak var lblAppNotification: UILabel!
+    
+    //MARK: Variables
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setColors()
+    }
+}
 
+extension SettingVC{
+
+    //MARK: Set Colors
+    func setColors(){
+        lblSetting.textColor = UIColor.customBlack
+        lblAccount.textColor = UIColor.customBlack
+        lblEditProfile.textColor = UIColor.customBlack
+        lblNotification.textColor = UIColor.customBlack
+        lblAppNotification.textColor = UIColor.customBlack
+        self.configureFonts()
+    }
+    
+    //MARK: Configure Fonts
+    func configureFonts() {
+        self.lblAppNotification.font = UIFont.MontserratMedium(16)
+        self.lblSetting.font = UIFont.MontserratMedium(24)
+        self.lblEditProfile.font = UIFont.MontserratMedium(16)
+        self.lblAccount.font = UIFont.MontserratSemiBold(20)
+        self.lblNotification.font = UIFont.MontserratSemiBold(20)
     }
 }
