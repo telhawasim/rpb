593b5f1af312e0e41363a9c0701d47b99c062eac
telha.wasim <telha.wasim@synavos.com>
2023-01-17 10:48:58 +0500

    Added localization in the app

---
+++ RPB/Presentation/Constant/Localization/Localization.swift
@@ -0,0 +1,8 @@
+//
+//  Localization.swift
+//  RPB
+//
+//  Created by Telha Wasim on 16/01/2023.
+//
+
+import Foundation
---
+++ RPB/Presentation/UIImages/UIImage.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage.swift
+//  RPB
+//
+//  Created by Telha Wasim on 16/01/2023.
+//
+
+import Foundation
